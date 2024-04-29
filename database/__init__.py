66ac81a64cbac6b8fdd185d6e5efdb5cb6f732bc
Eduardo Turcios <eddieturcios@outlook.com>
2024-04-29 08:31:51 -0400

    revert database

--- database/__init__.py
+++ database/__init__.py
@@ -1,7 +1,4 @@
 from pymongo import MongoClient
 
-mongo_client = MongoClient(
-    "mongodb+srv://eddieturcios112:Balamc12@cluster0.3jelkgt.mongodb.net/"
-)
-# mongo_client = MongoClient("database")
+mongo_client = MongoClient("database")
 db = mongo_client["cse312"]
